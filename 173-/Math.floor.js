ca6312e53d5343dca351248b001dc79ca0b16329
Eric <liyangzhong@hotmail.com>
2020-04-05 21:52:51 +0800

    update

---
+++ 173-/Math.floor.js
@@ -0,0 +1,5 @@
+function randomNumber(val) {
+    return Math.floor(Math.random() * val + 1 )
+}
+
+console.log(randomNumber(5)) // random number from one to five
---
+++ 173-/unary_plus.js
@@ -0,0 +1,6 @@
+console.log(+3 )    // 3
+console.log(+'3' )  // 3
+console.log(+true ) // 1
+console.log(+false) // 0
+console.log(+null)  // 0
+console.log(+function(val){ return val }) // NaN
